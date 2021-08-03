8ef06ef341eef4d7979c4d0fb42f3008d67b34e3
AlejandraIbarboA <alejandra.ibarbo@upb.edu.co>
2021-08-03 15:08:44 -0500

    Mejora codigo

--- Assets/Codigo.cs
+++ Assets/Codigo.cs
@@ -10,7 +10,8 @@ public class Codigo : MonoBehaviour
         
     }
     //Inserte codigo aqui :D
-    // Aqui hay mucho codigo importane 
+    // Aqui hay mucho codigo importane
+    // Mas codigo :D
     // Update is called once per frame
     void Update()
     {
