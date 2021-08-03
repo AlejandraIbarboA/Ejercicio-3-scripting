2525944dfab8bf4bb03ad3add2d6a8514d4f5bed
AlejandraIbarboA <87863253+AlejandraIbarboA@users.noreply.github.com>
2021-08-03 15:17:48 -0500

    Feature 1  (#1)
    * Codigo
    
    * Codigo mejorado
    
    * Mejora codigo

---
+++ Assets/Codigo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Codigo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //Inserte codigo aqui :D
+    // Aqui hay mucho codigo importane
+    // Mas codigo :D
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
